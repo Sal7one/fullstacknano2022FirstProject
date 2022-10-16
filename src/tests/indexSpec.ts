753f8aa3a3780d7d8fd2b208656eb3f548c205a1
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2022-10-16 04:46:17 +0300

    cleaned

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,10 @@
 import myFunc from './index'
 
-it('expect myFunc(5) to equal 25', () => {
-  expect(myFunc(5)).toEqual(25);
+describe('General test', () => {
+  it('expect myFunc(5) to equal 25', () => {
+    expect(myFunc(5)).toEqual(25);
+  });
+  it('expect myFunc(6) to equal 36', () => {
+    expect(myFunc(6)).toEqual(36);
+  });
 });
