2248b613fde11930e39a5f1eda1ce0088ef14064
Saleh Alanazi <Alanazi.saleh.77@gmail.com>
2022-10-18 15:46:59 +0300

    test updates

--- tests/indexSpec.ts
+++ tests/indexSpec.ts
@@ -1,36 +1,27 @@
 import supertest from 'supertest';
 import app from "../src/index"
 
-const request = supertest(app);
+    const request = supertest(app);
 
+    describe('Invalid parameters', ():void =>{
 
-  describe("Test App", function(){
-
-  describe('Test bad image width', (): void => {
         it('Response status 400 should be returned when sending bad value', async (): Promise<void> => {
             const response = await request.get(`/resizer?filename=fjord&height=123&width=ksa`);
             expect(response.status).toEqual(400);
         });
-    });
     
-    describe('Test with image that does not exisit', (): void => {
       it('Response status 400 should be returned when sending bad value', async (): Promise<void> => {
           const response = await request.get(`/resizer?filename=mynewimage&height=123&width=124`);
           expect(response.status).toEqual(400);
       });
-  });
-
-  describe('Test with image that goes well', (): void => {
     it('Response status 200 should be returned ', async (): Promise<void> => {
         const response = await request.get(`/resizer?filename=fjord&height=50&width=255`);
         expect(response.status).toEqual(200);
-    });
 });
 
-describe('404', (): void => {
     it('Response status 404 should be returned ', async (): Promise<void> => {
         const response = await request.get(`/randomPage`);
         expect(response.status).toEqual(404);
     });
-});
+
 });
