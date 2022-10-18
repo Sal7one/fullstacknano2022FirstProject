7b653167424ec9be5951e85e72474362700ac86b
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2022-10-18 08:37:40 +0300

    update tests

--- tests/units/imageFetcher.ts
+++
@@ -1,9 +0,0 @@
-import { promises as fs } from 'fs';
-
-async function imageFetcher(finalImageName: string): Promise<Buffer> {
-  const imagePath = __dirname + '\\modified\\' + finalImageName;
-  const imageBitmap = await fs.readFile(imagePath);
-  return imageBitmap;
-}
-
-export default imageFetcher;
