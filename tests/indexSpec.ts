ec5be79ed3e5433726952bfb9b9a9c4031d50a1a
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2022-10-19 03:07:19 +0300

    added spec reporter

--- tests/indexSpec.ts
+++ tests/indexSpec.ts
@@ -1,27 +1,51 @@
-import supertest from 'supertest';
-import app from "../src/index"
+import supertest from "supertest";
+import app from "../src/index";
+import {SpecReporter} from "jasmine-spec-reporter";
 
-    const request = supertest(app);
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(
+  new SpecReporter({
+    spec: {
+      displayPending: true,
+    },
+  })
+);
+const request = supertest(app);
 
-    describe('Invalid parameters', ():void =>{
+describe("Invalid parameters", (): void => {
+  it("Response status 400 should be returned when sending bad width", async (): Promise<void> => {
+    const response = await request.get(
+      `/resizer?filename=fjord&height=123&width=ksa`
+    );
+    expect(response.status).toEqual(400);
+  });
 
-        it('Response status 400 should be returned when sending bad value', async (): Promise<void> => {
-            const response = await request.get(`/resizer?filename=fjord&height=123&width=ksa`);
-            expect(response.status).toEqual(400);
-        });
-    
-      it('Response status 400 should be returned when sending bad value', async (): Promise<void> => {
-          const response = await request.get(`/resizer?filename=mynewimage&height=123&width=124`);
-          expect(response.status).toEqual(400);
-      });
-    it('Response status 200 should be returned ', async (): Promise<void> => {
-        const response = await request.get(`/resizer?filename=fjord&height=50&width=255`);
-        expect(response.status).toEqual(200);
+  it("Response status 400 should be returned when sending bad height", async (): Promise<void> => {
+    const response = await request.get(
+      `/resizer?filename=fjord&height=ddd&width=124`
+    );
+    expect(response.status).toEqual(400);
+  });
+  it("Response status 400 should be returned when sending a radom filename ", async (): Promise<void> => {
+    const response = await request.get(
+      `/resizer?filename=mynewimage&height=123&width=124`
+    );
+    expect(response.status).toEqual(400);
+  });
 });
 
-    it('Response status 404 should be returned ', async (): Promise<void> => {
-        const response = await request.get(`/randomPage`);
-        expect(response.status).toEqual(404);
-    });
+describe("Correct Request", (): void => {
+  it("Response status 200 should be returned ", async (): Promise<void> => {
+    const response = await request.get(
+      `/resizer?filename=fjord&height=50&width=255`
+    );
+    expect(response.status).toEqual(200);
+  });
+});
 
+describe("Random Page Request", (): void => {
+  it("Response status 404 should be returned ", async (): Promise<void> => {
+    const response = await request.get(`/randomPage`);
+    expect(response.status).toEqual(404);
+  });
 });
